ad7fb36dcec02300055bc55306f327fd7813b5d7
usernamemu <mhmdarif.developer@gmail.com>
2023-05-01 22:24:14 +0700

    feat: add flower desc

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,17 @@
 import { useState } from 'react'
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './App.css'
-import { NotFound } from './pages';
+import { 
+  NotFound,
+  FlowerDesc
+} from './pages';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="*" element={<NotFound />} />
+        <Route path="/flower-description" element={<FlowerDesc />} />
       </Routes>
     </BrowserRouter>
   )
--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -3,9 +3,9 @@ import { Link } from "react-router-dom";
 
 export default function NotFound() {
     return (
-        <div class="text-[#6D8B74] mx-auto w-auto h-screen flex flex-col gap-7 justify-center content-center">
-            <h1 class="text-8xl">Oops!</h1>
-            <p class="text-5xl">404 - Page Not Found</p>
+        <div className="text-[#6D8B74] mx-auto w-auto h-screen flex flex-col gap-7 justify-center content-center">
+            <h1 className="text-8xl">Oops!</h1>
+            <p className="text-5xl">404 - Page Not Found</p>
             
             <Link to='/'><button class="bg-[#6D8B74] text-white text-3xl px-5 py-2 rounded-md">HomePage</button></Link>
         </div>
