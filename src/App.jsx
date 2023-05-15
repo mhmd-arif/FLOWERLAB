950de3611e8ff363a2a63236522a8201974c9f09
usernamemu <mhmdarif.developer@gmail.com>
2023-05-15 19:57:15 +0700

    feat(Pages): add landing page

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,15 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './App.css'
 import { 
   NotFound,
-  FlowerDesc
+  FlowerDesc,
+  Home,
 } from './pages';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
+        <Route path="/" element={<Home />} />
         <Route path="*" element={<NotFound />} />
         <Route path="/flower-description" element={<FlowerDesc />} />
       </Routes>
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,9 @@
 import NotFound from "./NotFound";
 import FlowerDesc from "./FlowerDesc"
+import Home from "./Home";
 
 export{
     NotFound,
     FlowerDesc,
+    Home,
 };
