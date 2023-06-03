aa77e63235e6014c39af0de70fd38aa96d31365b
faizahbestiyana <faizahbestiyana@gmail.com>
2023-06-03 21:11:17 +0700

    fix some mistake

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,5 +1,7 @@
 import flower from "./flower img/flower.png"
+import user from "./flower img/user.png"
+import flowerSquare from "./flower img/flowerSquare.png"
 
 export{
-    flower,
+    flower, user, flowerSquare
 };
