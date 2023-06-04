48c514b82be7e2f6b64232c8e9218a4486b7fd45
faizahbestiyana <faizahbestiyana@gmail.com>
2023-06-04 23:12:39 +0700

    add path to signup and login page

--- src/pages/LoginPenjual.jsx
+++ src/pages/LoginPenjual.jsx
@@ -1,6 +1,7 @@
 /* eslint-disable react/no-unknown-property */
 import { flower, user } from "../assets"
 import { useState } from 'react';
+import { Link } from "react-router-dom";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faEye, faEyeSlash } from '@fortawesome/free-solid-svg-icons';
 
@@ -20,12 +21,12 @@ export default function LoginPenjual() {
             <div className="w-full md:w-1/2">
                 <h4 className="flex text-3xl ">Halo!</h4>
                 <label className="flex mt-5"> Email</label>
-                <input type="text" class="text-left mt-2 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5  dark:bg-[#FFFFFF] dark:border-gray-600 dark:placeholder-gray-600 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Masukkan email Anda" required />
+                <input type="text" class="text-left mt-2 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5  dark:bg-[#FFFFFF] dark:border-gray-600 dark:placeholder-gray-600 dark:text-black dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Masukkan email Anda" required />
                 <label className="flex mt-5"> Password</label>
                 <div className="relative">
                     <input
                         type={showPassword ? 'text' : 'password'}
-                        className="mt-2 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-[#FFFFFF] dark:border-gray-600 dark:placeholder-gray-600 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                        className="mt-2 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-[#FFFFFF] dark:border-gray-600 dark:placeholder-gray-600 dark:text-black dark:focus:ring-blue-500 dark:focus:border-blue-500"
                         placeholder="Masukkan password Anda"
                         required
                     />
@@ -39,7 +40,12 @@ export default function LoginPenjual() {
                 <div className="flex justify-end mt-5 mb-7">
                     <button className="bg-[#6D8B74] text-white text-1xl px-4 py-2 rounded-md">Masuk</button>
                 </div>
-                <label className="text-center mt-5"> Belum punya akun?  <span className="underline font-bold ml-2"> Daftar di sini!</span></label>
+                <label className="text-center mt-5"> Belum punya akun?</label>
+                <Link className="" to="/daftar-penjual">
+                    <span className="underline font-bold ml-2"> 
+                        Daftar di sini!
+                    </span>
+                </Link>
             </div>
         </div>
     </div>
