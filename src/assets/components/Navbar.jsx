e4d1731eb9f3cfb9f0e239cbad8271c58bc78f3b
mhmd-arif <mhmdarif.developer@gmail.com>
2023-06-06 02:00:50 +0700

    navbar

--- src/assets/components/Navbar.jsx
+++ src/assets/components/Navbar.jsx
@@ -1,15 +1,16 @@
 import { useState } from 'react';
-import {useNavigate} from 'react-router-dom'
+import {Link} from 'react-router-dom'
 
 export default function Navbar() {
   
     return (
-      <div className='bg-white flex justify-between '>
-        <div>
-
+      <div className='bg-white flex justify-between items-center px-[50px] h-[50px] bg-[#5F7161] text-white'>
+        <div className='font-extrabold'>
+            <Link to="/">FlowerLab</Link>
         </div>
-        <div>
-            
+        <div className='flex gap-7 font-medium'>
+            <Link to="/marketplace">marketplace</Link>
+            <Link to="/login-penjual">Login</Link>
         </div>
       </div>
     );
